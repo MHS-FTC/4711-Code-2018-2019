79937b4e9995ba901122e4d0bbc3a33f1480440d
Ethan Hampton <ethan.hampton@msd.oregonk-12.net>
2018-09-13 16:37:05 -0700

    Initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_API/Examples/SimpleRobot.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.FTC_API.Examples;
+
+import com.qualcomm.hardware.motors.NeveRest20Gearmotor;
+import com.qualcomm.robotcore.hardware.configuration.MotorConfigurationType;
+import org.firstinspires.ftc.teamcode.FTC_API.Robot.RobotBase;
+
+/**
+ * Created by Ethan Hampton on 8/19/17.
+ * <p>
+ * Just a simple robot that drives
+ */
+
+class SimpleRobot extends RobotBase {
+    Drive drive = new Drive()
+            .setMotorNames("Left_Motor", "Right_Motor")
+            .setMotorType(MotorConfigurationType.getMotorType(NeveRest20Gearmotor.class));
+
+    SimpleRobot() {
+        addSubSystem(drive);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/readme.md
@@ -0,0 +1,3 @@
+## Code for team 4711
+
+This is the team code for team 4711. Should be updated throughout the season
