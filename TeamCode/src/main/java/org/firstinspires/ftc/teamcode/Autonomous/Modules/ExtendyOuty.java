4c5564aac877a08746f5e0eeb70b6cc7f6018d9c
Ethan Hampton <ethan.hampton@msd.oregonk-12.net>
2019-02-02 15:17:02 -0800

    Change motion of arm in autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Modules/ExtendyOuty.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Modules/ExtendyOuty.java
@@ -6,13 +6,13 @@ import org.firstinspires.ftc.teamcode.Robot.Subsystems.Intake;
 public class ExtendyOuty extends Module {
     private double startTime;
 
-    private final int ENCODER_TICKS_TO_EXTEND = 500;
+    private final double ENCODER_ROTATIONS_TO_EXTEND = 0.7;
     private final int TIME_TO_WAIT = 5000;//wait 5 seconds
 
     @Override
     public void start() {
         Intake intake = (Intake) robot.getSubSystem("Intake");
-        intake.setArmTarget(ENCODER_TICKS_TO_EXTEND);
+        intake.setArmTarget((int) (ENCODER_ROTATIONS_TO_EXTEND * 16 * 1120));//16 to 1 ratio and 1120 encoder ticks for a 40 motor
         intake.driveArm(0);
         startTime = robot.getTimeMilliseconds();
     }
